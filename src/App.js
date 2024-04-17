eb394eee9192a55c6c9d1ce818d23671e17331e8
Abduqodir <AbduqodirDeveloper@gmail.com>
2024-04-17 12:03:51 +0500

    react_lesson-4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { image1, image2, image3, image4, image5, image6, image7, image8, image9 } from './assets/images';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header id='header'>
+        <nav id='navbar'>
+          <div className="container">
+            <div className="navbar_content">
+              <h1 className='marck-script-regular'>Олег Марков</h1>
+              <ul>
+                <li><a href="#">Главная</a></li>
+                <li><a href="#">Обо мне</a></li>
+                <li><a href="#">Связаться</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </header>
+      <main>
+        <div className="container-xl">
+          <div className="main_content">
+            <div className="main_content-grid item">
+              <img src={image1} alt="" />
+            </div>
+            <div className="main_content-grid item">
+              <img src={image4} alt="" />
+            </div>
+            <div className="main_content-grid item">
+              <img src={image7} alt="" />
+            </div>
+            <div className="main_content-grid item">
+              <img src={image2} alt="" />
+            </div>
+            <div className="main_content-grid item">
+              <img src={image5} alt="" />
+            </div>
+            <div className="main_content-grid item">
+              <img src={image8} alt="" />
+            </div>
+            <div className="main_content-grid item">
+              <img src={image3} alt="" />
+            </div>
+            <div className="main_content-grid item">
+              <img src={image6} alt="" />
+            </div>
+            <div className="main_content-grid item">
+              <img src={image9} alt="" />
+            </div>
+            
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
